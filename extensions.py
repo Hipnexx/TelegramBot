377fe3c4d986ac100ef7bbcb5f4a0793c4bcfcd7
Hipnexx <115487665+Hipnexx@users.noreply.github.com>
2022-11-16 22:46:13 +0300

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -28,7 +28,7 @@ class get_price:
         except ValueError:
             raise APIException(f'Не удалось обработать количество {amount}')
 
-        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={base_ticker}&tsyms={quote_ticker}')
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
         total_base = json.loads(r.content)[keys[base]]
 
         return total_base
