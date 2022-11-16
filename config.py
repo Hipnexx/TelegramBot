20c11074ec41e3364d719993f1e750f69a263cb3
Hipnexx <115487665+Hipnexx@users.noreply.github.com>
2022-11-16 21:55:53 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 TOKEN = '5573055516:AAHjXEN4b6tQ3zz52u4jknkn-Fvr_xcd8Vs'
 
 keys = {
-    'Dollar': 'USD',
-    'Bitcoin': 'BTC',
-    'Etherium': 'ETH',
+    'dollar': 'USD',
+    'bitcoin': 'BTC',
+    'etherium': 'ETH',
     'BUSD': 'BUSD',
-    'Tether': 'USDT',
+    'tether': 'USDT',
     'XRP': 'XRP',
-    'Chiliz': 'CHZ',
-    'Binance Coin': 'BNB',
+    'chiliz': 'CHZ',
+    'binance coin': 'BNB',
     'dogecoin': 'DOGE',
-    'Polygon': 'MATIC',
-    'Solana': 'SOL',
+    'polygon': 'MATIC',
+    'solana': 'SOL',
 }
